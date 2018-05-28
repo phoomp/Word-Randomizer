5e2d93c300ae302cda792881b3017f0779f5931e
Phoom Punpeng <phoomp14@icloud.com>
2018-05-28 13:13:53 +0700

    Initial Commit

---
+++ Word RandomizerTests/Word_RandomizerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Word_RandomizerTests.swift
+//  Word RandomizerTests
+//
+//  Created by Phoom Punpeng on 28/5/2561 BE.
+//  Copyright © 2561 Phoom Punpeng. All rights reserved.
+//
+
+import XCTest
+@testable import Word_Randomizer
+
+class Word_RandomizerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
